2c462691220e3b0219dda7ee2bfad214778f47b8
Matheus.Jagi <matheus.jagi@sicoob.com.br>
2024-10-21 23:50:29 -0300

    Fix stdins and readme

--- src/main/scala/conversion/main.scala
+++ src/main/scala/conversion/main.scala
@@ -1,17 +1,18 @@
 package conversion
 
+import scala.io.StdIn
+
 @main def run(): Unit =
     
     println("Informe o valor para conversao: ")
-    val value = scala.io.StdIn.readLine()
+    val value = StdIn.readLine()
 
     println("Informe a base do numero informado: ")
-    val readSourceBase = scala.io.StdIn.readLine()
-    val sourceBase: Int = readSourceBase.toInt
+    val sourceBase = StdIn.readInt()
 
     println("Informe a base que deseja converter: ")
-    val targetBase = scala.io.StdIn.readLine()
+    val targetBase = StdIn.readInt()
 
-    val resultConversion = conversionToBase(Integer.parseInt(value, sourceBase), sourceBase, targetBase.toInt)
+    val resultConversion = conversionToBase(Integer.parseInt(value.trim, sourceBase), sourceBase, targetBase)
 
-    println(s"O valor $value na base $sourceBase convertido para base $targetBase e: $resultConversion")
+    println(s"O valor ${value.trim} na base $sourceBase convertido para base $targetBase e: $resultConversion")
